79e5f28291066488b78c944000a1d54dc5fd0cf4
mmitacc <manuelmitacc@hotmail.com>
2023-03-02 18:51:01 -0500

    Finish component Videogames

--- api/src/Controllers/videogameController.js
+++ api/src/Controllers/videogameController.js
@@ -57,8 +57,8 @@ const getAllGames = async () => {
   const getAllApis = formatMyDb.concat(formatApiRawg);
   //Ordenando los Videogames por ID
   getAllApis.sort((a, b) => {
-    if (a.id > b.id) return 1;
-    if (a.id < b.id) return -1;
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
     return 0;
   });
 
--- client/src/components/videogames/Videogames.jsx
+++ client/src/components/videogames/Videogames.jsx
@@ -14,31 +14,58 @@ const Videogames = () => {
   }, []);
 
   //SELECCIONANDO TIPO DE BUSQUEDA CON ESTADO LOCAL DEL COMPONENTE:---------------------------------------------
-  const [order, setOrder] = useState("Videogame"); //Se define un estado local para Buscar nombres de "videoGames"
+  const [typeSearch, setTypeSearch] = useState("Videogame"); //Define un estado local para el tipo de busqueda
   const onChangeFilter = (event) => {
     //Reiniciar filtro de busquedas:
     setSearch("");
     setCurrentPage(0);
     //Recoge el valor de selección del filtro:
+    setTypeSearch(event.target.value);
+  };
+
+  //Ordenando Videogames por Alfabetico/Rating y Ascendente/Descendente:
+  const orderTypoMode = (tipo, modo) => {
+    console.log("TIPO====> ", tipo);
+    console.log("MODO====> ", modo);
+    if (tipo === "Alfabetico") {
+      videoGames.sort((a, b) => {
+        if (a.name > b.name) return 1 * modo;
+        if (a.name < b.name) return -1 * modo;
+        return 0;
+      });
+    } else if (tipo === "Rating") {
+      videoGames.sort((a, b) => {
+        if (a.rating > b.rating) return 1 * modo;
+        if (a.rating < b.rating) return -1 * modo;
+        return 0;
+      });
+    }
+  };
+
+  //SELECCIONANDO TIPO PARA ORDENAMIENTO CON ESTADO LOCAL DEL COMPONENTE:---------------------------------------
+  const [typeOrder, setTypeOrder] = useState("Alfabetico"); //Define un estado local para busquedas Alfabetico/Rating
+  const onChangeTypeOrder = (event) => {
+    orderTypoMode(event.target.value, order);
+    setTypeOrder(event.target.value);
+  };
+
+  //SELECCIONANDO MODO PARA ORDENAMIENTO CON ESTADO LOCAL DEL COMPONENTE:---------------------------------------
+  const [order, setOrder] = useState("1"); //Define un estado local para modo Ascendente(1)/Descendente(-1)
+  const onChangeOrder = (event) => {
+    orderTypoMode(typeOrder, event.target.value);
     setOrder(event.target.value);
   };
-  // Ordenando por NOMBRE de videoGames:
-  // videoGames.sort((a, b) => {
-  //   if (a.id > b.id) return 1;
-  //   if (a.id < b.id) return -1;
-  //   return 0;
-  // });
 
   //PAGINACIÓN CON ESTADO LOCAL DEL COMPONENTE:----------------------------------------------------
   const gamesXpag = 15; //Se establece el número de Videogames por Página a mostrar
   const [currentPage, setCurrentPage] = useState(0); //Se define un estado local para la Paginación
-  let dataVideogames = []; //Se define el array de Videogames a mostrar
+  let dataVideogames = [...videoGames]; //Se define el array de Videogames a mostrar
   const filterSearchName = () => {
     //Recorta solo "gamesXpag" Videogames para mostrar por página de la data guardada en "videoGames"
     if (search.length === 0) {
       dataVideogames = [...videoGames];
       return videoGames.slice(currentPage, currentPage + gamesXpag);
-    } else if (order === "Videogame") {
+    } else if (typeSearch === "Videogame") {
       let filterSearch = videoGames.filter((vg) => vg.name.includes(search));
       dataVideogames = [...filterSearch];
       return filterSearch.slice(currentPage, currentPage + gamesXpag);
@@ -71,46 +98,108 @@ const Videogames = () => {
 
   return (
     <div>
-      <div className="paginacion">
-        <label className="labelBlue">{" Filtrar por => "}</label>
-        <input
-          type="radio"
-          id="radio1"
-          value="Videogame"
-          checked={order === "Videogame" ? true : false}
-          onChange={onChangeFilter}
-        />
-        <label
-          className={order === "Videogame" ? "labelBlue" : null}
-          for="radio1"
-        >
-          Videogame
-        </label>
-        <input
-          type="radio"
-          id="radio2"
-          value="Genero"
-          checked={order === "Genero" ? true : false}
-          onChange={onChangeFilter}
-        />
-        <label className={order === "Genero" ? "labelBlue" : null} for="radio2">
-          Genero
-        </label>
-        <input
-          className="inputBlue"
-          type="text"
-          value={search}
-          placeholder="Escriba aquí..."
-          onChange={onSearchChange}
-        />
+      <div className="paginacion barra">
+        <div>
+          {/* Seccion de filtros por nombre y genero de Videogames */}
+          <label className="labelBlue">{" Filtrar por => "}</label>
+          <input
+            type="radio"
+            id="radio1"
+            value="Videogame"
+            checked={typeSearch === "Videogame" ? true : false}
+            onChange={onChangeFilter}
+          />
+          <label
+            className={typeSearch === "Videogame" ? "labelBlue" : null}
+            for="radio1"
+          >
+            Videogame
+          </label>
+          <input
+            type="radio"
+            id="radio2"
+            value="Genero"
+            checked={typeSearch === "Genero" ? true : false}
+            onChange={onChangeFilter}
+          />
+          <label
+            className={typeSearch === "Genero" ? "labelBlue" : null}
+            for="radio2"
+          >
+            Genero
+          </label>
+          <input
+            className="inputBlue"
+            type="text"
+            value={search}
+            placeholder="Escriba aquí..."
+            onChange={onSearchChange}
+          />
+        </div>
+        <div>
+          {/* Seccion de ORDENAMIENTO de Videogames: Alfabetico ó Rating*/}
+          <label className="labelBlue">{" Ordenar por orden => "}</label>
+          <input
+            type="radio"
+            id="radio3"
+            value="Alfabetico"
+            checked={typeOrder === "Alfabetico" ? true : false}
+            onChange={onChangeTypeOrder}
+          />
+          <label
+            className={typeOrder === "Alfabetico" ? "labelBlue" : null}
+            for="radio3"
+          >
+            Alfabetico
+          </label>
+          <input
+            type="radio"
+            id="radio4"
+            value="Rating"
+            checked={typeOrder === "Rating" ? true : false}
+            onChange={onChangeTypeOrder}
+          />
+          <label
+            className={typeOrder === "Rating" ? "labelBlue" : null}
+            for="radio4"
+          >
+            Rating
+          </label>
+          {/* Seccion de ORDENAMIENTO de Videogames: Ascendente ó Descendente*/}
+          <input
+            type="radio"
+            id="radio5"
+            value={1}
+            checked={order === "1" ? true : false}
+            onChange={onChangeOrder}
+          />
+          <label className={order === "1" ? "labelBlue" : null} for="radio5">
+            Ascendente
+          </label>
+          <input
+            type="radio"
+            id="radio6"
+            value={-1}
+            checked={order === "-1" ? true : false}
+            onChange={onChangeOrder}
+          />
+          <label className={order === "-1" ? "labelBlue" : null} for="radio">
+            Descendente
+          </label>
+        </div>
       </div>
-      <button className="blueButton" onClick={prevPage}>
-        Anteriores
-      </button>
-      <label>-- Página N° {currentPage / gamesXpag + 1} --</label>
-      <button className="blueButton" onClick={nextPage}>
-        Siguientes
-      </button>
+      {/* <hr className="lineHorizontal" /> */}
+      <div className="paginacion">
+        <button className="blueButton" onClick={prevPage}>
+          Anteriores
+        </button>
+        <label className="labelRed">
+          {"<--- Página N° "} {currentPage / gamesXpag + 1} {" --->"}
+        </label>
+        <button className="blueButton" onClick={nextPage}>
+          Siguientes
+        </button>
+      </div>
       <div className="containerVG">
         {filterSearchName().map((v, index) => {
           return (
