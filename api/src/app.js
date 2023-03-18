8a007f8b148beeb2799d601b1c1a9e071d5fdb7c
mmitacc <manuelmitacc@hotmail.com>
2023-03-18 00:12:20 -0500

    last settings for the deploy

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,10 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pi-videogames-main-three.vercel.app"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://pi-videogames-main-three.vercel.app"
+  ); // update to match the domain you will make the request from or "http://localhost:3000"
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import CreateVideogame from "./components/createVideogame/CreateVideogame.jsx";
 import VgameDetail from "./components/vgameDetail/VgameDetail.jsx";
 import Landing from "./components/landing/Landing";
 import NavBar from "./components/navBar/NavBar";
-export const ruthApp = "http://localhost:3001";
+export const ruthApp = "pi-videogames-main-production-5650.up.railway.app";
 
 function App() {
   const location = useLocation();
